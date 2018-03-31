fd11cc878d0ec8bc54eef1511839c6910f4aeb4f
Ola Vea <ola@olavea.com>
2018-03-31 14:16:36 +0200

    Create StorePicker.js

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+    render() {
+        return <p>Hallå! I am the StorePicker!</p>
+    }
+  }
+
+  export default StorePicker;
