a850bb5bd7991056917f6de16b77a6f70fbda871
Ola Vea <ola@olavea.com>
2018-04-09 14:31:32 +0200

    Update App. Video 13 Wes Bos
    Understanding State

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -5,29 +5,33 @@ import Inventory from './Inventory';
 
 
 class App extends React.Component {
-    state = {
-        fishes: {}, //empty object, could use string and... or...
+    state = {          // object,
+        fishes: {}, //empty object, could use string and... or...//empty state of fishes
         order: {} //empty object, use what you will end up using
     };
-    addFish = (fish) => { // how do you get a fish into form
-        this.setState   // takes in a fish. console.log("add a fish");
-
-
-
+    addFish = (fish) => { // how do you get a an item / fish into state console.log(adding a fish);
+   // takes in a fish. console.log("add a fish");
+//you cannot pdate dirkctley from this addFish form because
+//the methods that update state and the actual state always needs to live in the same component
+// video 7 state is the datas home props is the vw bus
+// we create this secondary method here addFish = (fish) => { 16:07
+//need to acces this
+// How do I actually call this method right?
+// Weird we wanna call it two levels deeper
+// we want to run it in add fish form
+// How does anything get into an component props and inventory
+      // this.setState
 
 
     };
-      //1 take a copy of the existing state, non-mutation
+      //1 take a copy of the existing state, non-mutation, non-deep-clone
       const fishes = { ...this.state.fishes};
 
       //2 add our new fish to that fishes variable
       fishes[`fish${Date.now()}`] = fish;
-
+      //... overwrite our exixting state and that will trigger a change in react
 
       //3 set the neew fishes object to state
-
-
-
 
       this.setState({
           fishes: fishes
