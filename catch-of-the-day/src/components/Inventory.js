da7384f03c82acfd7e2f323b17190d79fc3448ce
Ola Vea <ola@olavea.com>
2018-04-09 14:33:12 +0200

    Video 13 by Wes Bos

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -6,9 +6,9 @@ class Inventory extends React.Component{
       return (
         <div className="inventory">
           <h2>Inventory!</h2>
-          <form>
-          <AddFishForm addFish={this.props.addFish} /> // the fishes doesnt live in inventory
-          </form>
+
+          <AddFishForm addFish={this.props.addFish} /> // the fishes doesnt live in inventory, but it has been passed in via props. We have access to this function(?) which really lives two levels higher.
+
         </div>
       );
   }
